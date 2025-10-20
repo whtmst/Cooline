eae2c477fb0d6863d05af2aa62c360cec51d1a9e
Wht Mst <ven000mus@gmail.com>
2025-10-20 14:12:21 +0400

    Add files via upload

---
+++ settings.lua
@@ -0,0 +1,27 @@
+cooline_theme = {
+	vertical = false,
+	reverse = false,
+	width = 225,
+	height = 20,
+	statusbar = [[Interface\TargetingFrame\UI-StatusBar]],
+	bgcolor = { 0, 0, 0, 0.5 },
+	border = [[Interface\DialogFrame\UI-DialogBox-Border]],
+	bordersize = 16,
+	borderinset = 4,
+	bordercolor = { 1, 1, 1, 1 },
+	iconoutset = 2,
+	font = [[Fonts\ARIALN.TTF]],
+	fontsize = 10,
+	fontcolor = { 1, 1, 1, 0.8 },
+	spellcolor = { 0.8, 0.4, 0, 1 },
+	nospellcolor = { 0, 0, 0, 1 },
+	inactivealpha = 0.5,
+	activealpha = 1.0,
+	treshold = 3.0,
+}
+
+cooline_ignore_list = {
+	'hearthstone'
+}
+
+COOLINE_LOADED_MESSAGE = 'Cooline loaded: move the location of the cooline bar by holding <alt> while dragging it with left mouse button.'
